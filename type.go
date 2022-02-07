54f10bf6c39a8654a3656866ac3441b5fbc8f950
Risky Feryansyah <36788585+RiskyFeryansyahP@users.noreply.github.com>
2022-02-07 16:14:03 +0700

    feat: add find index method and create new interface (#2)

---
+++ type.go
@@ -0,0 +1,28 @@
+package main
+
+type QueueStr interface {
+	// Enqueue add new element add store it in rear / end
+	// position of the queue
+	Enqueue(value string)
+
+	// Dequeue get element at front of the queue
+	// and remove the element at front of the queue
+	Dequeue() string
+
+	// Length get the size of the queue
+	Length() int
+
+	// IsEmpty check if the queue is empty
+	IsEmpty() bool
+
+	// HeadValue get the front / head value of the queue
+	HeadValue() string
+
+	// TailValue get the rear / tail value of the queue
+	TailValue() string
+
+	// FindIndex find the index of given value as param
+	// and return the index if value matches with value
+	// in the queue
+	FindIndex(value string) (int, bool)
+}
