11574897704199128310250e7c7582877adca388
Risky Feryansyah Pribadi <riskypribadi24@gmail.com>
2022-02-09 16:42:53 +0700

    feat: add implementation queue with `int64` data type

--- type.go
+++ type.go
@@ -80,3 +80,30 @@ type QueueInt32 interface {
 	// in the queue
 	FindIndex(value int32) (int, bool)
 }
+
+type QueueInt64 interface {
+	// Enqueue add new element add store it in rear / end
+	// position of the queue
+	Enqueue(value int64)
+
+	// Dequeue get element at front of the queue
+	// and remove the element at front of the queue
+	Dequeue() int64
+
+	// Length get the size of the queue
+	Length() int
+
+	// IsEmpty check if the queue is empty
+	IsEmpty() bool
+
+	// HeadValue get the front / head value of the queue
+	HeadValue() int64
+
+	// TailValue get the rear / tail value of the queue
+	TailValue() int64
+
+	// FindIndex find the index of given value as param
+	// and return the index if value matches with value
+	// in the queue
+	FindIndex(value int64) (int, bool)
+}
